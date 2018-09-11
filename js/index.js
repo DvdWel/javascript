eb1ad861bfa2ebb90bbb171c25e086a76e8220d3
DvdWel <dvdwel@hotmail.nl>
2018-09-11 13:14:57 +0200

    changed some js spacing

--- js/index.js
+++ js/index.js
@@ -38,8 +38,12 @@ document.addEventListener('keydown', (event) => {
 function showSlides(n) {
 	var i;
 	var slides = document.getElementsByClassName("slide");
-	if (n > slides.length) {slideIndex = 1} 
-	if (n < 1) {slideIndex = slides.length}
+	if (n > slides.length) {
+		slideIndex = 1
+	} 
+	if (n < 1) {
+		slideIndex = slides.length
+	}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
