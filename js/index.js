ce0577c353eca14f8196846b6ad98c3fd85c2f2f
DvdWel <dvdwel@hotmail.nl>
2018-09-11 11:36:00 +0200

    final build of slider js-es5

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
 "use strict";
 
-
-
 var slideIndex = 1;
+var paused;
 showSlides(slideIndex);
 
+var timer = setInterval(function () {
+	plusSlides(1);
+}, 2000);
 
-setInterval(function(){plusSlides(1);}, 2000);
+function startTimer(){
+	clearInterval(timer);
+	
+	timer = setInterval(function () {
+		plusSlides(1);
+	}, 2000);
+}
 
+function pauseSlideShow(){
+	if(paused == true){
+		plusSlides(1);
+		startTimer();
+		paused = false;
+	}else{
+		clearInterval(timer);
+		paused = true;
+	}
+}
 
 // Next/previous controls
 function plusSlides(n) {
@@ -19,30 +37,30 @@ function currentSlide(n) {
 	showSlides(slideIndex = n);
 }
 
-/*document.onkeydown(test(event));
-
-function test(event){
-	var x = event.keyCode;
-	console.log(x);
-}*/
-
-document.addEventListener('keydown', (event) => {
-
-	if(event.key == "ArrowLeft"){
+document.addEventListener('keydown', function (event) {
+	console.log(event.key);
+	startTimer();
+	if (event.key == "ArrowLeft") {
 		plusSlides(-1);
-	}else if(event.key == "ArrowRight"){
+	} else if (event.key == "ArrowRight") {
 		plusSlides(1);
+	}else if(event.key == " "){
+		pauseSlideShow();
 	}
 });
 
 function showSlides(n) {
 	var i;
 	var slides = document.getElementsByClassName("slide");
-	if (n > slides.length) {slideIndex = 1} 
-	if (n < 1) {slideIndex = slides.length}
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
 	for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
 	}
 
-  slides[slideIndex-1].style.display = "block";
+	slides[slideIndex - 1].style.display = "block";
 }
