77bdee5c9fb043d089bb1b31fcd5a2a0d1f1c994
DvdWel <dvdwel@hotmail.nl>
2018-09-17 11:43:10 +0200

    added a shadow to the footer

--- js/index.js
+++ js/index.js
@@ -1,19 +1,30 @@
-
-
-const apiPromise = fetch('https://randomuser.me/api/?results=5&nat=nl');
+const ul = document.getElementById('mainList');
+let contactsAmount = 20;
+const apiPromise = fetch(`https://randomuser.me/api/?results=${contactsAmount}&nat=nl`);
 
 apiPromise
 .then(data => data.json())
 .then(data => { 
 	const people = data.results;
 	people.forEach(person => {
-		console.log(person);
-		let name = person.name.first + " " + person.name.last;
-		console.log(name);
+		let fullName = person.name.first + " " + person.name.last;
 		let phone = person.phone;
-		console.log(phone);
-		let 
+		let pic = person.picture.medium;
+
+		const list = document.createElement('li');
+		const img = document.createElement('img');
+		const name = document.createElement('span');
+		const phoneNumber = document.createElement('p');
+
+		ul.appendChild(list);
+		list.appendChild(img);
+		img.src = pic;
 		
+		list.appendChild(name);
+		name.innerText = fullName;
+		
+		name.appendChild(phoneNumber);
+		phoneNumber.innerText = phone;
 	});
  })
 
